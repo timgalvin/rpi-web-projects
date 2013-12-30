958a0a502f125d17fa8c3c856b70c48423d8d5bf
Tim Galvin <tgalvin@uw.edu>
2013-12-29 22:20:32 -0800

    Adding DB php code

--- recipes.php
+++ recipes.php
@@ -73,7 +73,31 @@
         <p>
           <a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">View navbar docs &raquo;</a>
         </p>
+	<?php
+            mysql_connect("localhost","root","root") or die(mysql_error());
+            mysql_select_db("maindb") or die(mysql_error());
+
+            $result = mysql_query("SELECT * FROM contacts") or die(mysql_error());
+            
+            echo "<table border='1'>";
+            echo "<tr><th>First Name</th><th>Last Name</th><th>Phone</th><th>Email</th><th>Favorite Color</th></tr>";
+            while($row = mysql_fetch_array($result)) {
+                echo "<tr><td>";
+                echo $row['first_name'];
+                echo "</td><td>";
+                echo $row['last_name'];
+                echo "</td><td>";
+                echo $row['phone'];
+                echo "</td><td>";
+                echo $row['email'];
+                echo "</td><td>";
+                echo $row['favorite_color'];
+                echo "</td></tr>";
+            }
+            echo "</table>";
+        ?>
       </div>
+
 
     </div> <!-- /container -->
 
