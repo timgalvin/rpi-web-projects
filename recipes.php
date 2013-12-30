f7ccd39e55245cde8a0dacbef96f96b59103dab6
Tim Galvin <tgalvin@uw.edu>
2013-12-29 23:53:52 -0800

    -mFormatting

--- recipes.php
+++ recipes.php
@@ -38,8 +38,8 @@
         </div>
         <div class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
+            <li><a href="index.html">Home</a></li>
+            <li class="active"><a href="#">Recipes</a></li>
             <li><a href="#contact">Contact</a></li>
             <li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
