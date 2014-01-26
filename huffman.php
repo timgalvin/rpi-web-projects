3aa3a26e91c96823415bc04381a1e5dbaabdb14d
Tim <tgalvin@uw.edu>
2014-01-25 18:20:20 -0800

    work on huffman

--- huffman.php
+++ huffman.php
@@ -1,4 +1,4 @@
 <?php
-    $output = shell_exec('/home/pi/huffman-translator/huffman "Argument"');
+    $output = shell_exec('/home/pi/huffman-translator/huffman "Arguments for days"');
     echo $output;
 ?>
