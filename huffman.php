75f28c302dabc8b66e03f2a6e1ca71145be504f0
Tim <tgalvin@uw.edu>
2014-01-28 00:23:26 -0800

    editing huffman page

--- huffman.php
+++ huffman.php
@@ -89,7 +89,7 @@
                   <div class="form-group">
                     <label for="textArea" class="col-lg-2 control-label">Input Text</label>
                     <div class="col-lg-10">
-                      <textarea class="form-control" rows="3" id="textArea"></textarea>
+                      <textarea class="form-control" rows="10" id="textArea"></textarea>
                       <span class="help-block">Enter up to 5000 characters to encode.</span>
                     </div>
                   </div>
